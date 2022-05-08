1d7fc724df6bfa47e77173ba9b349fad8edd2125
Dirk Robinson <tiaan.robinson@gmail.com>
2022-05-08 12:58:17 +0200

    Results

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,27 @@ fn print_verified(account: &BankAccount) {
     println!("{:?}", account.verified);
 }
 
+fn is_verified(account: &BankAccount) -> Result<bool, bool> {
+    match account.verified {
+        true => Ok(true),
+        false => Err(false)
+    }
+}
+
 fn main() {
     let my_account = BankAccount {
         balance: 20,
-        verified: true
+        verified: false
     };
+    let verification_status = is_verified(&my_account)
+        .unwrap(); //gets the value from a successful result
+
+    let _verification_status_2 = is_verified(&my_account)
+        .expect("Unable to unwrap results!");
 
     print_balance(&my_account);
     print_verified(&my_account);
+    println!("{:?}", verification_status);
 
     //println!("{:?}", my_account.balance);
     //println!("{:?}", my_account.verified);
